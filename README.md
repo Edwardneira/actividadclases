07fca3fc775ffcda42299081bfe351fb7d068979
edwardneira <edneiraa32@gmail.com>
2024-04-03 21:30:48 -0500

    creacionrepositorioactividad

---
+++ README.md
@@ -0,0 +1,27 @@
+## GUÍA CONCEPTOS BÁSICOS DE JAVASCRIPT
+
+A continuación estaremos desarrollando el paso a paso para conectar mi repositorio local (el código en mi computador) con mi repositorio remoto (mi biblioteca en GitHub)
+
+1. Crear el repositorio en GitHub (remoto)
+   *buscamos el botón verde con ícono de libro que dice nuevo
+   *llenamos el nombre del repositorio, descrpción (opcional) y se deja en PUBLICO!!!!!
+   *Click en el botón verde crear repositorio
+
+2.Crear nuestro repositorio local
+ *abrimos la consola de gitbash desde visual:
+    1.ctrl+ñ
+    2.parte superior Terminal y nuevo Terminal
+
+*vamos a escribir los comandos para incializar nuestro repositorio local y conectarlo con el remoto
+COMANDOS
+1.git init: inicializar mi repositorio local 
+    1.1 git status: muestra el estado de mis archivos
+2.git add: se usa para agragar cambios al entorno de preparacion
+3.git comit: nos permite poner un mensaje per ademas significa que sus archivos estan listos para enviarse al repositorio remoto
+
+
+
+ ##QUE PONER EN EL README PARA MI PROYECTO FINAL
+ 1. Titulo -> PsicoKids
+ 2. Descrpción general que me hable de todo el proyecto
+ 3. Autor -> Quién lo hizo-Mi nombre
---
+++ conceptoBasico.js
@@ -0,0 +1,30 @@
+// .js SE UTULIZA PARA COMENTAR EN JS
+// EL CONSOLE.LOG ES LA FORMA DE ENVIAR MENSAJES A LA CONSOLA (CONSOLE) DEL NAVEGADOR
+console.log("hola, mundo y psicolgos");
+
+//CONCEPTOS BASICOS EN JS (JAVASCRPT)
+  //1. tipos de variables
+  //2. manejo del DOM
+  //3. condicionales
+  //4. ciclos
+
+//LAS VARIABLES EN JS (JAVASCRIPT): (las variables son contenedores de informacion)
+//hay 3 tipos de variables ne JS pero solo vamos a usa 2
+// VAR: es un tipo de variable que ya no se usa pero aun funcina NO SE USA!! (cambia en el tiempo)
+// LET: es una variable que cambia en el tiempo es decir que yo puedo decidir cambiar el resultado
+// CONST: es una variable que no puede cambiar en el tiempo 
+
+//TIPOS DE DATOS:
+// NOTA: el signo = no es comparativo sino que asigna valores
+//NUMERICOS:
+let numero = 7; //(importante cerrar con ;)
+// CADENA DE CARACTERES:
+let texto = "hola mundo";
+//BOOLEANOS: es un dato que puede ser verdadero o falso
+let esVerdadero = true; //(verdad)
+let esFalso = false; // (flaso)
+//ARREGLOS O ARRAYS (en programacion): 
+//me permiten almacenar varios datos al mismo tiempo en una misma variable
+let arregloNombres = ["Luisa", "Emmanuel", "Paula"];
+let arregloNumeros = [1, 2, 3, 4,];
+  
