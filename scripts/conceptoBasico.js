d854690dd909e20c946a7085e07935a4487b9dfc
edwardneira <edneiraa32@gmail.com>
2024-04-10 20:57:46 -0500

    práctica de los conceptos

--- scripts/conceptoBasico.js
+++ scripts/conceptoBasico.js
@@ -27,4 +27,24 @@ let esFalso = false; // (flaso)
 //me permiten almacenar varios datos al mismo tiempo en una misma variable
 let arregloNombres = ["Luisa", "Emmanuel", "Paula"];
 let arregloNumeros = [1, 2, 3, 4,];
-  
+//OBJETOSs: son "una representacion de elementos de la vida real peroen nuestro codigo
+//pueden tener:
+//atributos: (caracteristicas)
+//metodos: acciones que puede realizar el objeto
+//(en JS son para ejecutar acciones o invocar funciones)
+//[son para declarar arreglos]
+//{son para declarar objetos}
+const celular = {
+    color:"blanco",
+    modelo:"galaxy a7",
+    ancho:350,
+    alto:800,
+    enviarMensajes: function(){}
+    //entre los {} al lado de () se establece la accion a realiar
+  };
+
+
+//en la carpeta scripts van todos los archivos de JS pero destinados
+//a darle dinamismo al sitio web, NO se relacionan con la funcionalidad a nivel del backend
+//NODE JS se va a utilizar para implementar el JS en el backend (backend son las operaciones logicas
+// que permiten el funcionamiento del sitio web) sin JS no funciona el sitio web
---
+++ scripts/practicaConceptos.js
@@ -0,0 +1,9 @@
+//1.verificar que este conectado al html
+console.log("Hola, soy la actividad de hoy");
+//2.Traer los elemenos que se necesitan del html. los que tienen id.
+//los elemos que traiga deben estar en una variable y esa variable
+//debe ser constante
+
+//Documento: me accede al html GetelemnyById:me trae in id en particular o nombrado
+const edadInput = document.getElementById("edadInput");
+console.log(edadInput);
