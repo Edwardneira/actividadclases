67278747d6e746e003948379ba82d5534d7c1e67
edwardneira <edneiraa32@gmail.com>
2024-04-16 21:09:27 -0500

    ajustes realizados clase 15abr

--- scripts/practicaConceptos.js
+++ scripts/practicaConceptos.js
@@ -9,8 +9,10 @@ console.log("Hola, soy la actividad de hoy 15abr");
 const edadInput = document.getElementById("edadInput");
 console.log(edadInput);
 //elemento2 1ra forma (se lleva directamente el elemento al JS)
-const btn = document.getElementById("boton");
-console.log(btn);
+               //const btn = document.getElementById("boton");
+               //console.log(btn);
+const mensaje = document.getElementById ("mensajeEdad");
+console.log(mensaje);
 //elemento2 2da forma (cada que se le de click sale mensaje "Hola")
 function verificarEdad(){
     console.log("Entró a mi funcion verificar edad");
@@ -18,10 +20,10 @@ function verificarEdad(){
     console.log(edad);
     if(edad>= 18){
         console.log("Eres mayor de edad");
+        mensaje.textContent = "Eres mayor de edad";
     } else{
-        console.log("Eres menor de edad");
+        console.log("Eres menor de edad"); //else define que sucede
+        //cuando la condicion es falsa (sinó)
+        mensaje.textContent = "Eres menor de edad";
     }
 }
-//elemento3
-const mensaje = document.getElementById("mensajeEdad");
-console.log("");
